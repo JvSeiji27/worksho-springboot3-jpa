cc1e95dccaa6a65ea6fd3028ecc4d4f053eacd10
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-06-25 20:45:38 -0300

    Service layer, component registration

--- src/main/java/com/edcandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/edcandoweb/course/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.edcandoweb.course.entities.User;
 //Possibilita a utilização do CRUD;
 
+//@Repository --> mas como esta herdando do JpaRepository, já é componente registrado
 public interface UserRepository extends JpaRepository<User, Long>{
 //JPA já tem uma implementação, não preciso implementar;
 }

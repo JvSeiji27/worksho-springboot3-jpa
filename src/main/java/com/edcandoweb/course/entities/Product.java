5cf3b2b91a07fd1ab4d1fdb00b0b9ebd532e6485
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-06-26 20:55:39 -0300

    Many to many association with JoinTable

--- src/main/java/com/edcandoweb/course/entities/Product.java
+++ src/main/java/com/edcandoweb/course/entities/Product.java
@@ -11,6 +11,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -27,8 +31,12 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set <Category> categories = new HashSet<>();
+	
 	@Transient
 	private List <Order> orders = new ArrayList<>();
 	
@@ -96,9 +104,13 @@ public class Product implements Serializable{
 		return orders;
 	}
 
-	public void setOrders(List<Order> orders) {
-		this.orders = orders;
+
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
+
+
 
 	@Override
 	public boolean equals(Object obj) {
